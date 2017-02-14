de88b0d1dba29325d13de928ddb5fede6702ab0c
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-02-14 11:40:31 +0900

    アプリ名変更

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_three_models_session'
+Rails.application.config.session_store :cookie_store, key: '_rails_blog_session'
