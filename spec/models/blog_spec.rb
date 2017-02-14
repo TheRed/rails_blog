010a69e24df417fa5a841611e776daa43b1c3e5d
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-02-14 16:59:19 +0900

    spec修正
    title: nil と明記した

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -6,7 +6,7 @@ describe Blog do
     expect(blog.valid?).to be_truthy
   end
   it "titleがなければ無効な状態であること" do
-    blog = Blog.new
+    blog = Blog.new(title: nil)
     expect(blog.valid?).to be_falsy
   end
 end
