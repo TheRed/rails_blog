a91cf4ccb02f1890e0207945f695f7cb3031ac0a
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-02-17 11:19:02 +0900

    ルーティング作成
    entriesのindexは除外

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :blogs do
+    resources :entries, except: :index
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
