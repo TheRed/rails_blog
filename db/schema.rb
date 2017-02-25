af4b705923ab294b9ffc35418187d9d64b7990c3
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-02-25 15:57:53 +0900

    Comment.statusのデフォルト値を'unapproved'に設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202192136) do
+ActiveRecord::Schema.define(version: 20170225064954) do
+
+  create_table "another_blogs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -20,8 +27,21 @@ ActiveRecord::Schema.define(version: 20170202192136) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
-    t.string   "status"
+    t.string   "status",     default: "unapproved", null: false
     t.integer  "entry_id"
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "company_memberships", force: :cascade do |t|
+    t.integer  "company_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -30,6 +50,12 @@ ActiveRecord::Schema.define(version: 20170202192136) do
     t.string   "title"
     t.text     "body"
     t.integer  "blog_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
