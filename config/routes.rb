f0f60d6c1fef556c8eb931f3535f4a9503f4d501
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-02-25 14:59:52 +0900

    まずentriesと同じ仕様でcommentsのCRUD画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :blogs do
-    resources :entries, except: :index
+    resources :entries, except: :index do
+      resources :comments, except: :index
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
