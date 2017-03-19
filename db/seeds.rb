79aeea8ae02c7c5b09a6e880b559926f5558d6ee
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-03-19 18:36:05 +0900

    Add initialize to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+blog_cat = Blog.find_or_create_by!(id: 1, title: 'ねこがすき！にゃんにゃんブログ')
+blog_dog = Blog.find_or_create_by!(id: 2, title: 'いぬがすき！わんわんブログ')
+blog_ito = Blog.find_or_create_by!(id: 3, title: 'つまがすき！いとうさんブログ')
+
+entry_1st = blog_cat.entries.find_or_create_by!(id: 1, title: 'はじめてのエントリー', body: 'はじめまして！')
+entry_2nd = blog_cat.entries.find_or_create_by!(id: 2, title: '2番目のエントリー',    body: 'おひさしぶりです！')
+entry_orz = blog_ito.entries.find_or_create_by!(id: 3, title: '3番目のエントリー',    body: 'もうくじけました・・・')
+
+entry_1st.comments.find_or_create_by!(id: 1, body: 'てすてす',               status: 'approved')
+entry_1st.comments.find_or_create_by!(id: 2, body: 'ねこはかわいいですよね', status: 'unapproved')
+entry_2nd.comments.find_or_create_by!(id: 3, body: '例のねこについて',       status: 'approved')
+entry_orz.comments.find_or_create_by!(id: 4, body: 'こんにちはこんにちは！', status: 'approved')
